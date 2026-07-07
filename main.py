bec54a4da50e1752b577cc2a2157915b4adeeb62
NikitaGamza <gamza-ns@mail.ru>
2026-07-07 15:29:41 +0400

    feat: cov

--- main.py
+++ main.py
@@ -48,15 +48,15 @@ if __name__ == "__main__":
     print(product1.price)
     print(product1.quantity)
 
-    # print(product2.name)
-    # print(product2.description)
-    # print(product2.price)
-    # print(product2.quantity)
-    #
-    # print(product3.name)
-    # print(product3.description)
-    # print(product3.price)
-    # print(product3.quantity)
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print(product3.name)
+    print(product3.description)
+    print(product3.price)
+    print(product3.quantity)
 
     category1 = Category(
         "Смартфоны",
@@ -73,16 +73,15 @@ if __name__ == "__main__":
     print(cat1.category_count)
     print(cat1.product_count)
 
-    # product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    # category2 = Category("Телевизоры",
-    #                      "Современный телевизор, который позволяет наслаждаться просмотром,
-    #                      станет вашим другом и помощником",
-    #                      [product4])
-    #
-    # print(category2.name)
-    # print(category2.description)
-    # print(len(category2.products))
-    # print(category2.products)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category2 = Category("Телевизоры",
+                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+                         [product4])
 
-    # print(Category.category_count)
-    # print(Category.product_count)
+    print(category2.name)
+    print(category2.description)
+    print(len(category2.products))
+    print(category2.products)
+
+    print(Category.category_count)
+    print(Category.product_count)
