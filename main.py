79f7cb4d136975a7010c3fcf7e4b7c513392b9e3
NikitaGamza <gamza-ns@mail.ru>
2026-08-12 17:12:18 +0400

    feat: Smartphone subclass

--- main.py
+++ main.py
@@ -66,6 +66,16 @@ class Product:
             print("Цена не должна быть нулевая или отрицательная")
 
 
+class Smartphone(Product):
+    """Класс смартфонов"""
+    def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: int, model: str, memory: int, color: str):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
 class Category:
     """Класс категорий продукта"""
 
